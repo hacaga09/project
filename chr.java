77ea135cfe187f019095de16a6584860ba985a9b
hacaga09 <hacaga.qaraev@mail.ru>
2026-01-28 01:33:37 +0300

    Update chr.java

--- chr.java
+++ chr.java
@@ -1,4 +1,4 @@
-package com.example.game;
+package com.example.gameproject;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -6,21 +6,21 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class chr {
-    public static ImageView im = new ImageView(new Image("C:\\Users\\hacag\\OneDrive\\Рабочий стол\\перс1.png"));
-    public static double X = 960-70; public static double Y = 540-100;
-    public static double Xc = X+im.getImage().getWidth()/2; public static double Yc = Y+im.getImage().getHeight()/2;
+    public static ImageView im=new ImageView(new Image("C:\\Users\\hacag\\OneDrive\\Рабочий стол\\перс1.png"));
+
+    public static double X; public static double Y;
+    public static double Xc; public static double Yc;
     public static double rot;
-    public static int speed = 17;
+    public static int speed=12;
 
-    public static double hwidth = 98; public static double hheight = 154;
-    public static Rectangle hitbox = new Rectangle(X,Y,hwidth,hheight);
-
+    public static double hwidth=98;
+    public static double hheight=154;
+    public static Rectangle hitbox=new Rectangle(0, 0, hwidth, hheight);
 
     public static void start(Stage stage){
         hitbox.setVisible(false);
-
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args){
     }
 }
