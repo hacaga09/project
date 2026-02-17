1b45e51d91dca77c923344e87980997148768fad
hacaga09 <hacaga.qaraev@mail.ru>
2026-02-18 00:32:56 +0300

    Update wall.java

--- wall.java
+++ wall.java
@@ -3,6 +3,27 @@ package com.example.gameproject;
 import javafx.scene.shape.Rectangle;
 
 public class wall {
-    public static Rectangle lowerWall = new Rectangle(13, 897, 933, 14);
+                                                //left-right, up-down, w, h
+    public static Rectangle w1 = new Rectangle(13, 897, 933, 14);    //w,h
+    public static Rectangle w2 = new Rectangle(13, 233, 12, 664);
+    public static Rectangle w3 = new Rectangle(25, 233, 133, 12);
+    public static Rectangle w4 = new Rectangle(158, 183, 12, 344);
+    public static Rectangle w5 = new Rectangle(170, 183, 269, 12);
+    public static Rectangle w6 = new Rectangle(439, 0, 12, 353);
+    public static Rectangle w7 = new Rectangle(451, 0, 159, 8);
+    public static Rectangle w8 = new Rectangle(610, 0, 12, 528);
+    public static Rectangle w9 = new Rectangle(622, 183, 321, 12);
+    public static Rectangle w10 = new Rectangle(931, 195, 12, 332);
+    public static Rectangle w11 = new Rectangle(809, 514, 125, 15);
+    public static Rectangle w12 = new Rectangle(870, 529, 12, 90);
+    public static Rectangle w13 = new Rectangle(607, 619, 338, 12);
+    public static Rectangle w14 = new Rectangle(931, 631, 12, 267);
+    public static Rectangle w15 = new Rectangle(328, 690, 12, 207);
+    public static Rectangle w16 = new Rectangle(607, 848, 12, 49);
+    public static Rectangle w17 = new Rectangle(607, 631, 12, 154);
+    /*public static Rectangle w14 = new Rectangle(931, 631, 12, 267);
+    public static Rectangle w14 = new Rectangle(931, 631, 12, 267);
+    */
+
 
 }
