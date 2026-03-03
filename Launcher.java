ab3e5d9c4904652a145c6f9f8086ba745ee86d10
hacaga09 <hacaga.qaraev@mail.ru>
2026-03-04 02:12:30 +0300

    .

---
+++ Launcher.java
@@ -0,0 +1,7 @@
+package com.example.gameproject;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
---
+++ gameProject/src/main/java/com/example/gameproject/wall.java
@@ -0,0 +1,36 @@
+package com.example.gameproject;
+
+import javafx.scene.shape.Rectangle;
+
+public class wall {
+                                                //left-right, up-down, w, h
+    public static Rectangle w1 = new Rectangle(13, 897, 933, 14);    //w,h
+    public static Rectangle w2 = new Rectangle(13, 233, 12, 664);
+    public static Rectangle w3 = new Rectangle(25, 233, 133, 12);
+    public static Rectangle w4 = new Rectangle(158, 183, 12, 344);
+    public static Rectangle w5 = new Rectangle(170, 183, 269, 12);
+    public static Rectangle w6 = new Rectangle(439, 0, 12, 349);
+    public static Rectangle w7 = new Rectangle(451, 0, 159, 8);
+    public static Rectangle w8 = new Rectangle(610, 0, 12, 528);
+    public static Rectangle w9 = new Rectangle(622, 183, 321, 12);
+    public static Rectangle w10 = new Rectangle(931, 195, 12, 332);
+    public static Rectangle w11 = new Rectangle(809, 514, 125, 15);
+    public static Rectangle w12 = new Rectangle(870, 529, 12, 90);
+    public static Rectangle w13 = new Rectangle(607, 619, 338, 12);
+    public static Rectangle w14 = new Rectangle(931, 631, 12, 267);
+    public static Rectangle w15 = new Rectangle(328, 692, 12, 205);
+    public static Rectangle w16 = new Rectangle(607, 848, 12, 49);
+    public static Rectangle w17 = new Rectangle(607, 631, 12, 154);
+    public static Rectangle w18 = new Rectangle(329, 527, 12, 100);
+    public static Rectangle w19 = new Rectangle(25, 516, 40, 12);
+    public static Rectangle w20 = new Rectangle(125, 514, 82, 12);
+    public static Rectangle w21 = new Rectangle(273, 515, 176, 12);
+    public static Rectangle w22 = new Rectangle(439,415, 12, 111);
+    public static Rectangle w23 = new Rectangle(452, 238, 42, 12);
+    public static Rectangle w24 = new Rectangle(554, 237, 55, 12);
+    public static Rectangle w25 = new Rectangle(610, 517, 139, 12);
+    public static Rectangle FINAL = new Rectangle(494,240,60, 5);
+
+
+
+}
